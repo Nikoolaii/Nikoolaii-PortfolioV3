6b624aba4e4bbdc9439fb5606a36de6bc54f3019
Nikoolaii <nikolailemerre@gmail.com>
2024-10-29 10:54:03 +0100

    :sparkles: Done post functionnality

--- components/posts/GetSix.vue
+++ components/posts/GetSix.vue
@@ -3,13 +3,20 @@
     class="text-2xl text-violet-500 dark:text-violet-400 border-b-2 border-violet-500 dark:border-violet-400 mb-4 font-bold">
     Posts</h1>
   <div class="grid grid-cols-3 gap-4">
-    <ContentList path="/posts" v-slot="{ list }">
-      <div v-for="post in sortedPosts(list)" :key="post._path" class="col-span-1">
-        <UCard>
+    <ContentList v-slot="{ list }" :query="{
+      sort: [{ date: -1 }],
+      limit: 6
+    }" path="/posts">
+      <div v-for="post in list" :key="post._path" class="flex flex-col">
+        <UCard class="flex flex-col flex-1">
           <template #header>
-            <h2>{{ post.title }}</h2>
+            <h2 class="font-bold text-violet-500 dark:text-violet-400">{{ post.title }}</h2>
+            <h2>{{ formatDate(post.date) }}</h2>
           </template>
-          <p>{{ post.description }}</p>
+          <p class="flex-1 min-h-[70px]">{{ post.description }}</p>
+          <template #footer>
+            <ULink :to="post._path">Lire plus</ULink>
+          </template>
         </UCard>
       </div>
     </ContentList>
@@ -19,8 +26,12 @@
 <script>
 export default {
   methods: {
-    sortedPosts(posts) {
-      return posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)).slice(0, 6);
+    formatDate(date) {
+      return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      }).format(new Date(date));
     }
   }
 }
