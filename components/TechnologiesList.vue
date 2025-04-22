981e3e064aef84929300ed1f7ffa9cec92265b28
Nikoolaii <nikolailemerre@gmail.com>
2025-04-22 16:19:33 +0200

    ♻️ Refactor TechnologiesList component and implement custom scroll handling

--- components/TechnologiesList.vue
+++ components/TechnologiesList.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { techList } from '~/data/tech';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, onUnmounted } from 'vue';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -8,21 +8,48 @@ gsap.registerPlugin(ScrollTrigger);
 
 const techIconsContainer = ref(null);
 
+const isDivInViewport = (el) => {
+  if (!el) return false;
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.bottom <= window.innerHeight
+  );
+};
+
+const handleGlobalWheel = (e) => {
+  const el = techIconsContainer.value;
+  if (!el) return;
+
+  // Si la div est visible à l'écran
+  if (isDivInViewport(el) && el.scrollWidth > el.clientWidth) {
+    const delta = e.deltaY || e.deltaX;
+
+    // Détection en cours de scroll de la div
+    console.log('scrolling techIconsContainer');
+
+    // Scroll vers la droite (descend)
+    if (delta > 0) {
+      if (el.scrollLeft + el.clientWidth < el.scrollWidth) {
+        e.preventDefault();
+        el.scrollLeft += delta;
+      }
+    }
+    // Scroll vers la gauche (remonte)
+    else if (delta < 0) {
+      if (el.scrollLeft > 0) {
+        e.preventDefault();
+        el.scrollLeft += delta;
+      }
+    }
+  }
+};
+
 onMounted(() => {
-  ScrollTrigger.create({
-    // trigger: techIconsContainer.value,
-    // start: 'top top',
-    // end: () => `+=${techIconsContainer.value.scrollWidth - techIconsContainer.value.clientWidth}`,
-    // pin: true,
-    // scrub: true,
-    // onUpdate: self => {
-    //   if (self.progress === 1) {
-    //     ScrollTrigger.getAll().forEach(trigger => trigger.kill());
-    //   }
-    // }
-    // Send a message to the console when the container is scrolled
-    onUpdate: self => console.log(self.progress)
-  });
+  window.addEventListener('wheel', handleGlobalWheel, { passive: false });
+});
+onUnmounted(() => {
+  window.removeEventListener('wheel', handleGlobalWheel);
 });
 </script>
 
@@ -32,7 +59,7 @@ onMounted(() => {
       class="text-2xl text-violet-500 dark:text-violet-400 border-b-2 border-violet-500 dark:border-violet-400 mb-4 font-bold">
       {{ $t('technologies.title') }}
     </h1>
-    <div ref="techIconsContainer" class="tech-icons-container flex whitespace-nowrap overflow-auto">
+    <div ref="techIconsContainer" class="tech-icons-container flex whitespace-nowrap overflow-hidden">
       <UTooltip v-for="(item, index) in techList" :key="index" :text="item.name" :delay="500"
         :popper="{ arrow: true, placement: 'top' }">
         <UIcon :name="item.icon" class="tech-icon mr-2 mb-2 h-24 w-24" />
